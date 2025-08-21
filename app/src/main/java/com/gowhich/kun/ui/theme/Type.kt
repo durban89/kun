8b404f25b3c5c18bae58b7bc0bbdf1afb7fa0a61
zhangdapeng <zhangdapeng89@126.com>
2025-08-21 15:05:57 +0800

    update appbar

---
+++ app/src/main/java/com/gowhich/kun/ui/theme/Type.kt
@@ -0,0 +1,46 @@
+package com.gowhich.kun.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.gowhich.kun.R
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+val AbrilFatface = FontFamily(
+    Font(R.font.abrilfatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+
+)
+
+val Typography = Typography (
+    bodyLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+
+    ),
+
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+
+    displaySmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+)
