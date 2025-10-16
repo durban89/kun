5671e2ba1d60f763284a92bc3649da261044e285
zhangdapeng <zhangdapeng89@126.com>
2025-10-16 16:07:01 +0800

    update nav

--- app/src/main/java/com/gowhich/kun/MainActivity.kt
+++ app/src/main/java/com/gowhich/kun/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.gowhich.kun
 
-import android.graphics.Paint.Style
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -24,6 +23,11 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
 
 //import com.gowhich.kun.ui.theme.KunTheme
 
@@ -83,9 +87,27 @@ class MainActivity: ComponentActivity() {
 }
 
 
+
 @Preview
 @Composable
 fun MainContainer() {
+
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("detail") {
+            DetailScreen(navController)
+        }
+    }
+
+}
+
+// 首页
+@Composable
+fun HomeScreen(navController: NavController) {
     val fontOffset = Offset(5f, 10f)
     Column(
         Modifier
@@ -115,8 +137,8 @@ fun MainContainer() {
                 verticalAlignment = Alignment.CenterVertically
 
             ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Text("返回1s")
+                Button(onClick = { navController.navigate("detail") }) {
+                    Text("详情页")
                 }
 
                 Text(
@@ -139,7 +161,38 @@ fun MainContainer() {
         MessageList()
 
     }
+//
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+//    ) {
+//        Text("Home Screen")
+//        Button(onClick = {
+//            // 导航到详情页
+//            navController.navigate("detail")
+//        }) {
+//            Text("Go to Detail")
+//        }
+//    }
+}
 
+// 详情页
+@Composable
+fun DetailScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Detail Screen")
+        Button(onClick = {
+            // 返回上一页
+            navController.popBackStack()
+        }) {
+            Text("Go Back")
+        }
+    }
 }
 
 @Composable
